a028b1eccb427df5def64c589cd653b052a483dc
abderahmandege <adege63@gmail.com>
2020-06-28 10:33:36 -0500

    c

--- src/test/java/first.java
+++ src/test/java/first.java
@@ -2,6 +2,7 @@ public class first {
     public static void main(String[] args) {
         System.out.println("clone");
         System.out.println("first push");
+        System.out.println("onemore");
     }
     
 }
